4f9fd2e0f6d2fcd674c5c767aaf5f4f35a254902
Hermann <edgar.poe1994@gmail.com>
2022-12-30 22:57:33 +0200

    add info about user

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('It steel work!');
+const userName = prompt('Enter your name:')
+const userAge = prompt('How old are you?')
+const userFrom = prompt('Where are you from?')
+
+const message = `Hello ${userName}! You are ${userAge} years old. You are from ${userFrom}.`;
+
+console.log(message);
