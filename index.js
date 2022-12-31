220094f4e18f7f7a5e4fdb93e5d6718d6f0dec52
Hermann <edgar.poe1994@gmail.com>
2022-12-31 11:14:12 +0200

    add questionnaire

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('It steel work!');
+const userName = prompt('Enter your name:')
+const userAge = prompt('How old are you?');
+const smoke = confirm('Are you smoke?');
+
+if(userAge < 18) {
+    if (smoke) {
+        alert(`Hello ${userName}! Shame on you!`);
+    } else {
+        alert(`Hello ${userName}! Well done!`);
+    }
+} else { 
+    if (smoke) {
+        alert(`Hello ${userName}! Spend your money on something else!`);
+    } else {
+        alert(`Hello ${userName}! You are doing fine!`);
+    }
+}
