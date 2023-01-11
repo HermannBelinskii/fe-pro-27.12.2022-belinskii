e75ad4b81d8ff04bc7bccd4fb7665fa232bdd086
Hermann <edgar.poe1994@gmail.com>
2023-01-11 16:59:52 +0200

    add hw6

--- index.js
+++ index.js
@@ -1,55 +1,47 @@
 let userDate = +prompt(`Якого ти року народження?`);
-let userFrom = prompt(`В якому місті ти живешь?`);
-let userSport = prompt (`Яким твій улюблений вид спорту?`);
 let userAge;
-let famousSportsmen;
+
+if (userDate) {
+    userAge = `Тобі зараз ${2023 - userDate} років!`;
+    alert (userAge)
+} else {
+    alert (`Шкода, що ти не захотів ввести свій рік народження!`)
+} 
+
+let userFrom = prompt(`В якому місті ти живешь?`);
 let userCity;
 
-// switch (userSport) {
-//     case 'Футбол':
-//         famousSportsmen = `Твій улюбленний вид спорту - ${userSport} . Круто! Хочешь стати як Ліонель Месси?`
-//         break;
-//     case 'Бокс':
-//         famousSportsmen = `Твій улюбленний вид спорту - ${userSport} . Круто! Хочешь стати як Олександр Усік?`
-//         break;
-//     case 'Теніс':
-//         famousSportsmen = `Твій улюбленний вид спорту - ${userSport} . Круто! Хочешь стати як Джиммі Конорс?`
-//         break;
-//     case null:
-//         famousSportsmen = `Шкода, що ти не захотів ввести свій улюблений вид спорту!`
-//         break;
-//     default: 
-//         famousSportsmen = `Твій улюбленний вид спорту - ${userSport} . Круто!`
-// }
+if (userFrom === 'Київ') {
+    userCity = `Ти живеш у столиці України!`;
+    alert (userCity);
+} else if (userFrom === 'Вашингтон') {
+    userCity = `Ти живеш у столиці USA!`;
+    alert (userCity);
+} else if (userFrom === 'Лондон') {
+    userCity = `Ти живеш у столиці Великої Британії!`;
+    alert (userCity);
+} else if (!userFrom) {
+    alert (`Шкода, що ти не захотів ввести своє місто!`);
+} else {
+    userCity =`Ти живеш у місті ${userFrom}!`;
+    alert (userCity);
+}
+
+let userSport = prompt (`Яким твій улюблений вид спорту?`);
+let famousSportsmen;
 
 if (userSport === 'Футбол') {
-    famousSportsmen = `Твій улюбленний вид спорту - ${userSport} . Круто! Хочешь стати як Ліонель Месси?`
+    famousSportsmen = `Круто! Хочешь стати як Ліонель Месси?`;
+    alert (famousSportsmen);
 } else if (userSport === 'Бокс') {
-    famousSportsmen = `Твій улюбленний вид спорту - ${userSport} . Круто! Хочешь стати як Олександр Усік?`
+    famousSportsmen = `Круто! Хочешь стати як Олександр Усік?`;
+    alert (famousSportsmen);
 } else if (userSport === 'Теніс') {
-    famousSportsmen = `Твій улюбленний вид спорту - ${userSport} . Круто! Хочешь стати як Джиммі Конорс?`
+    famousSportsmen = `Круто! Хочешь стати як Джиммі Конорс?`;
+    alert (famousSportsmen);
 } else if (!userSport) {
-    famousSportsmen = `Шкода, що ти не захотів ввести свій улюблений вид спорту!`
+    alert (`Шкода, що ти не захотів ввести свій улюблений вид спорту!`);
 } else {
-    famousSportsmen =`Твій улюбленний вид спорту - ${userSport} . Круто!`
+    famousSportsmen =`Твій улюбленний вид спорту - ${userSport} . Круто!`;
+    alert (famousSportsmen);
 }
-
-if (userFrom === 'Київ') {
-    userCity = `Ти живеш у столиці України!`
-} else if (userFrom === 'Вашингтон') {
-    userCity = `Ти живеш у столиці USA!`
-} else if (userFrom === 'Лондон') {
-    userCity = `Ти живеш у столиці Великої Британії!`
-} else if (!userFrom) {
-    userCity = `Шкода, що ти не захотів ввести своє місто!`
-} else {
-    userCity =`Ти живеш у місті ${userFrom}!`
-}
-
-if (!userDate) {
-    userAge = `Шкода, що ти не захотів ввести свій рік народження!`;
-} else {
-    userAge = `Тобі зараз ${2023 - userDate} років!`;
-}
-
-alert (`Привіт! ${userAge} ${userCity} ${famousSportsmen}`)
