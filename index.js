2be34a4533ffcd6e85d4b4a8e038f368bbc5d000
Hermann <edgar.poe1994@gmail.com>
2023-01-25 19:00:05 +0200

    add hw9.2

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-let array = [1, 2, 3, 4, 5, 6, 7];
-console.log(array);
-function removeElement (item) {
-  return array.filter(function(el) {
+const array = [1, 2, 3, 4, 5, 6, 7];
+function removeElement (arr, item) {
+  return arr.filter(function(el) {
       return el !== item;
   })
 }
-console.log(removeElement(5));
-array = removeElement(5);
-console.log(array);
-
+console.log(removeElement(array, 5));
 
 
 // function removeElement (arr, item) {
