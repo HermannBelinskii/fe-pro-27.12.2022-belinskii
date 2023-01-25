f385106e74b6b306d374e01fea605be92522306e
Hermann <edgar.poe1994@gmail.com>
2023-01-25 18:10:33 +0200

    add hw10.1

--- index.js
+++ index.js
@@ -10,3 +10,18 @@ function generateKey(arr, num) {
 
 const key = generateKey(characters, 16);
 console.log(key);
+
+
+// const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+// function generateKey(arr, num) {
+//   let textFun = '';
+//   for (var i = 0; i < num; i++)
+//     textFun += arr[Math.floor(Math.random() * arr.length)];
+
+//   return textFun;
+// }
+
+// const key = generateKey(characters, 16);
+// console.log(key);
+
