7f0217c5100768135023b49ae300cf6196a4435d
Hermann <edgar.poe1994@gmail.com>
2023-01-25 17:51:02 +0200

    add hw10

---
+++ index.js
@@ -0,0 +1,12 @@
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+function generateKey(arr, num) {
+  let textFun = '';
+  for (var i = 0; i < num; i++)
+    textFun += arr.charAt(Math.floor(Math.random() * arr.length));
+
+  return textFun;
+}
+
+const key = generateKey(characters, 16);
+console.log(key);
