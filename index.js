3af97a1ea9f894acd4928563915afc8825eaf1af
Hermann <edgar.poe1994@gmail.com>
2023-02-22 18:21:35 +0200

    add hw14

---
+++ index.js
@@ -0,0 +1,131 @@
+class Student { 
+  constructor (firstName, lastName, dateOfBirth, grade, attendance){ 
+    this.firstName = firstName; 
+    this.lastName = lastName; 
+    this.dateOfBirth = dateOfBirth; 
+    this.grade = grade;  
+    this.attendance = new Array(25); 
+  } 
+   
+  calculateAge() { 
+    console.log(new Date().getFullYear() - this.dateOfBirth); 
+  } 
+   
+  calculateAverageScore(){ 
+    let averageScore = (this.grade.reduce((sum, mark) => sum + mark, 0)) / this.grade.length; 
+    return averageScore 
+  } 
+  
+  present(){ 
+    const index = this.attendance.findIndex(el => el === undefined); 
+    if (index !== -1) { 
+      this.attendance[index] = true; 
+    } else { 
+      console.log('Місця для запису вже закінчились'); 
+    } 
+  }   
+   
+  absent(){ 
+    const index = this.attendance.findIndex(el => el === undefined); 
+    if (index !== -1) { 
+      this.attendance[index] = false; 
+    } else { 
+      console.log('Місця для запису вже закінчились'); 
+    } 
+  } 
+  
+  calculateAverageAttendance () { 
+  const totalAttendance = this.attendance.filter(element => element !== false && element !== undefined).length;
+    let resultOfTotalAttendance = totalAttendance / this.attendance.length; 
+    return resultOfTotalAttendance;
+  } 
+  
+  summary() {
+    const attendance = this.calculateAverageAttendance();
+    const score = this.calculateAverageScore();
+    if (attendance >= 0.9 && score >= 90) {
+      console.log('Дуже добре!');
+    } else if (attendance >= 0.9 || score >= 90) {
+      console.log('Не погано');
+    } else {
+      console.log('Дуже погано!');
+    }
+  }
+} 
+
+let studentOne = new Student('Hermann', 'Belinskii', 1994, [90, 90, 90])
+
+studentOne.absent()
+studentOne.absent()
+studentOne.absent()
+studentOne.absent()
+studentOne.absent()
+studentOne.absent()
+studentOne.absent()
+studentOne.absent()
+studentOne.absent()
+studentOne.absent()
+studentOne.absent()
+studentOne.absent()
+studentOne.absent()
+studentOne.absent()
+studentOne.absent()
+studentOne.absent()
+studentOne.absent()
+studentOne.absent()
+studentOne.absent()
+studentOne.absent()
+studentOne.absent()
+studentOne.absent()
+studentOne.absent()
+studentOne.absent()
+studentOne.absent()
+
+console.log(studentOne)
+studentOne.calculateAverageScore()
+studentOne.summary()
+studentOne.calculateAge()
+console.log(studentOne.calculateAverageScore())
+
+
+let studentTwo = new Student('Alex', 'Fog', 1992, [100, 100, 90])
+
+studentTwo.present()
+studentTwo.present()
+studentTwo.present()
+studentTwo.present()
+studentTwo.present()
+studentTwo.present()
+studentTwo.present()
+studentTwo.present()
+studentTwo.present()
+studentTwo.present()
+studentTwo.present()
+studentTwo.present()
+studentTwo.present()
+studentTwo.present()
+studentTwo.present()
+studentTwo.present()
+studentTwo.present()
+studentTwo.present()
+studentTwo.present()
+studentTwo.present()
+studentTwo.present()
+studentTwo.present()
+studentTwo.present()
+studentTwo.present()
+studentTwo.present()
+
+console.log(studentTwo)
+studentTwo.calculateAverageScore()
+studentTwo.summary()
+studentTwo.calculateAge()
+console.log(studentTwo.calculateAverageScore())
+
+let studentThree = new Student('Olga', 'Love', 1992, [20, 20, 20])
+
+console.log(studentThree)
+studentThree.calculateAverageScore()
+studentThree.calculateAge()
+studentThree.summary()
+console.log(studentThree.calculateAverageScore())
