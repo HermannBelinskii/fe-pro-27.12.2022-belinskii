1ac175f34406d078b3d0fde14747320efb013d1a
Hermann <edgar.poe1994@gmail.com>
2023-03-08 17:57:25 +0200

    add pracktic

--- js/index.js
+++ js/index.js
@@ -54,52 +54,142 @@
 // })
 
 // ТАБЫ
-const tabsItem = document.querySelectorAll('.tab__item');
-const tabsText = document.querySelectorAll('.tab__text');
+// const tabsItem = document.querySelectorAll('.tab__item');
+// const tabsText = document.querySelectorAll('.tab__text');
 
-function toggleActiveClass(elements, activeClass, index) {
-    elements.forEach((el) => el.classList.remove(activeClass));
-    elements[index].classList.add(activeClass);
-}
+// function toggleActiveClass(elements, activeClass, index) {
+//     elements.forEach((el) => el.classList.remove(activeClass));
+//     elements[index].classList.add(activeClass);
+// }
     
-tabsItem.forEach((el, index) => {
-    el.addEventListener('click', function (e) {
-        toggleActiveClass(tabsItem, 'active__tab', index);
-        toggleActiveClass(tabsText, 'tab__text-active', index);
-    });
-});
+// tabsItem.forEach((el, index) => {
+//     el.addEventListener('click', function (e) {
+//         toggleActiveClass(tabsItem, 'active__tab', index);
+//         toggleActiveClass(tabsText, 'tab__text-active', index);
+//     });
+// });
 
 
 // ПОИСК
-const input = document.querySelector('.input-text');
-const list = document.querySelectorAll('.item-text')
+// const input = document.querySelector('.input-text');
+// const list = document.querySelectorAll('.item-text')
 
-input.addEventListener('input', (e)=>{
-    const valueInput = e.target.value;
-    list.forEach(el =>{
-        if(el.textContent.toLowerCase().indexOf(valueInput.toLowerCase()) > -1){
-            el.style.display = 'block';
-        } else {
-            el.style.display = 'none ';
-        }
-    })
+// input.addEventListener('input', (e)=>{
+//     const valueInput = e.target.value;
+//     list.forEach(el =>{
+//         if(el.textContent.toLowerCase().indexOf(valueInput.toLowerCase()) > -1){
+//             el.style.display = 'block';
+//         } else {
+//             el.style.display = 'none ';
+//         }
+//     })
 
-})
+// })
 
 
 
 // БОКОВОЕ МЕНЮ
 
-const openBtn = document.querySelector('.menu-btn');
-const menuList = document.querySelector('.sidebar');
-const closeBtn = document.querySelector('.close-sidebar');
+// const openBtn = document.querySelector('.menu-btn');
+// const menuList = document.querySelector('.sidebar');
+// const closeBtn = document.querySelector('.close-sidebar');
 
-openBtn.addEventListener('click', ()=>{
-    menuList.classList.add('show-sidebar')
-    closeBtn.style.display = 'block'
-})
+// openBtn.addEventListener('click', ()=>{
+//     menuList.classList.add('show-sidebar')
+//     closeBtn.style.display = 'block'
+// })
 
-closeBtn.addEventListener('click', ()=>{
-    menuList.classList.remove('show-sidebar')
-    closeBtn.style.display = 'none'
-})
+// closeBtn.addEventListener('click', ()=>{
+//     menuList.classList.remove('show-sidebar')
+//     closeBtn.style.display = 'none'
+// })
+
+// ACCORDEON
+
+// const btnAccordion = document.querySelectorAll('.accordion');
+// btnAccordion.forEach((el)=>{
+//     el.addEventListener('click', ()=>{
+//         let content = el.nextElementSibling;
+//         if(content.style.maxHeight){
+//             document.querySelectorAll('.content').forEach((el) => el.style.maxHeight = null);
+//         } else {
+//             document.querySelectorAll('.content').forEach((el)=> el.style.maxHeight = null);
+//             content.style.maxHeight = content.scrollHeight + 'px';;
+//         }
+//     })
+// })
+
+
+// const qfa = document.querySelectorAll('.fqa')
+// qfa.forEach(el => {
+//     el.addEventListener('click', () => {
+//         el.classList.toggle('active');
+//     })
+// })
+
+
+// // const openPopUpById = document.getElementById('open_pop_up');
+// const openPopUpByClass = document.querySelector('.open-pop-up');
+// // const closePopUpById = document.getElementById('close_pop_up');
+// const closePopUpByClass = document.querySelector('.pop-up__close');
+// const popUp = document.querySelector('.pop-up')
+
+// openPopUpByClass.addEventListener('click', function(e){
+//     e.preventDefault();
+//     popUp.classList.add('active');
+// })
+
+// closePopUpByClass.addEventListener('click', ()=>{
+//     popUp.classList.remove('active');
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
