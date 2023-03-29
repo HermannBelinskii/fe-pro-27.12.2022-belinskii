2e1354c480f679448ff44d126c87c8bdb92371fc
Hermann <edgar.poe1994@gmail.com>
2023-03-29 22:59:29 +0300

    add hw18-belinskii-3

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const form = document.querySelector('form');
 const postContainer = document.querySelector('#post-container');
+const URL = 'https://jsonplaceholder.typicode.com/posts/'
 
 form.addEventListener('submit', event => {
   event.preventDefault();
@@ -7,7 +8,7 @@ form.addEventListener('submit', event => {
   const postId = document.querySelector('#postId').value;
   if (!postId) return;
 
-  fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+  fetch(`${URL}${postId}`)
     .then(res => {
       if (res.ok) {
         return res.json(); 
@@ -21,47 +22,110 @@ form.addEventListener('submit', event => {
         <p>${post.body}</p>
       `;
       postContainer.appendChild(postElement);
-      
-      const commentsButton = document.createElement('button');
-      commentsButton.textContent = 'Find comments';
-      const closeButton = document.createElement('button');
-      closeButton.textContent = 'Close';
-      closeButton.addEventListener('click', ()=>{
-        postContainer.innerHTML = '';
-      })
-      commentsButton.addEventListener('click',  () => {
-        commentsButton.disabled = true;
-
-        fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`)
-          .then((res) => res.json())
-          .then(comments => {
-            const commentsElement = document.createElement('ul');
-            comments.forEach(comment => {
-              const commentItem = document.createElement('li');
-              commentItem.innerHTML = `
-                <h3>${comment.name}</h3>
-                <p>${comment.body}</p>
-              `;
-              commentsElement.appendChild(commentItem);
-            });
-            postElement.appendChild(commentsElement);
-          })
-          .catch(error => {
-            console.error(error);
-          })});
-      postElement.appendChild(commentsButton);
-      postElement.appendChild(closeButton);
+      postElement.appendChild(createCommentsButton(postId, postElement));
+      postElement.appendChild(createCloseButton());
     })
     .catch(error => {
       console.error(error.massege);
       createElementForMiss()
-  })
+    })
 });
 
-function createElementForMiss(){
-  const errorMass = document.createElement('p')
-  errorMass.innerText = 'You entered the wrong post number. Enter a number from 1 to 100.'
-  postContainer.appendChild(errorMass)  
-};
+
+function createCommentsButton(postId, postElement) {
+  const commentsButton = document.createElement('button');
+  commentsButton.textContent = 'Find comments';
+  commentsButton.addEventListener('click',  () => {
+    commentsButton.disabled = true;
+    fetch(`${URL}${postId}/comments`)
+      .then((res) => res.json())
+      .then(comments => {
+        const commentsElement = document.createElement('ul');
+        comments.forEach(comment => {
+          const commentItem = document.createElement('li');
+          commentItem.innerHTML = `
+            <h3>${comment.name}</h3>
+            <p>${comment.body}</p>
+          `;
+          commentsElement.appendChild(commentItem);
+        });
+        postElement.appendChild(commentsElement);
+      })
+      .catch(error => {
+        console.error(error);
+      })
+  });
+  return commentsButton;
+}
+
+function createCloseButton() {
+  const closeButton = document.createElement('button');
+  closeButton.textContent = 'Close';
+  closeButton.addEventListener('click', ()=>{
+    postContainer.innerHTML = '';
+  });
+  return closeButton;
+}
 
 
+
+// form.addEventListener('submit', event => {
+//   event.preventDefault();
+//   postContainer.innerHTML = '';
+//   const postId = document.querySelector('#postId').value;
+//   if (!postId) return;
+
+//   fetch(`${URL}${postId}`)
+//     .then(res => {
+//       if (res.ok) {
+//         return res.json(); 
+//       } 
+//       throw {massege: 'The post could not be found'};
+//     })
+//     .then(post => {
+//       const postElement = document.createElement('div');
+//       postElement.innerHTML = `
+//         <h2>${post.title}</h2>
+//         <p>${post.body}</p>
+//       `;
+//       postContainer.appendChild(postElement);
+//       const commentsButton = document.createElement('button');
+//       commentsButton.textContent = 'Find comments';
+//       const closeButton = document.createElement('button');
+//       closeButton.textContent = 'Close';
+//       closeButton.addEventListener('click', ()=>{
+//         postContainer.innerHTML = '';
+//       })
+//       commentsButton.addEventListener('click',  () => {
+//         commentsButton.disabled = true;
+//         fetch(`${URL}${postId}/comments`)
+//           .then((res) => res.json())
+//           .then(comments => {
+//             const commentsElement = document.createElement('ul');
+//             comments.forEach(comment => {
+//               const commentItem = document.createElement('li');
+//               commentItem.innerHTML = `
+//                 <h3>${comment.name}</h3>
+//                 <p>${comment.body}</p>
+//               `;
+//               commentsElement.appendChild(commentItem);
+//             });
+//             postElement.appendChild(commentsElement);
+//           })
+//           .catch(error => {
+//             console.error(error);
+//           })});
+//       postElement.appendChild(commentsButton);
+//       postElement.appendChild(closeButton);
+//     })
+//     .catch(error => {
+//       console.error(error.massege);
+//       createElementForMiss()
+//   })
+// });
+
+// function createElementForMiss(){
+//   const errorMass = document.createElement('p')
+//   errorMass.innerText = 'You entered the wrong post number. Enter a number from 1 to 100.'
+//   postContainer.appendChild(errorMass)  
+// };
